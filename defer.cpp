bbd7077906bfab130ab2e6e3c48170facaf70e2f
Carter Olsen <cjolsen06@gmail.com>
2025-07-12 07:50:45 -0700

    Added main files to the repo

---
+++ defer.cpp
@@ -0,0 +1,1 @@
+#include "defer/defer.hpp"
---
+++ include/defer/defer.hpp
@@ -0,0 +1,33 @@
+#ifndef DEFER_INCLUDE_DEFER_DEFER_H
+#define DEFER_INCLUDE_DEFER_DEFER_H
+
+#include <cstdio>
+#include <utility>
+
+template <typename Func>
+class DeferredCallback {
+  public:
+    DeferredCallback(Func&& function)
+      : callback(std::forward<Func>(function))
+      {}
+
+    ~DeferredCallback() {
+      callback();
+    }
+    
+  private:
+    Func callback;
+};
+
+template<typename Func>
+DeferredCallback<Func> make_deferred_callback(Func&& callback) {
+  return DeferredCallback(std::forward<Func>(callback));
+}
+
+#define DEFER_UNIQUE_NAME(a, b) DEFER_CONCAT(a, b)
+#define DEFER_CONCAT(a, b) a##b
+
+#define defer_inline(code) auto DEFER_UNIQUE_NAME(_deferred_, __LINE__) = make_deferred_callback([&]{ code });
+#define defer(func) auto DEFER_UNIQUE_NAME(_deferred_, __LINE__) = make_deferred_callback(func);
+
+#endif
