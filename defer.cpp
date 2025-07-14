8cfa54ddbbfdaa0d65958195125666f3621697b1
Carter Olsen <cjolsen06@gmail.com>
2025-07-14 07:35:35 -0700

    Added a couple small tests to defer.cpp to make sure that the defer features worked

--- defer.cpp
+++ defer.cpp
@@ -1,1 +1,12 @@
 #include "defer/defer.hpp"
+
+#include <cstdio>
+
+void nothing() { printf("Hello world\n"); }
+
+void something() {
+  int a = 0;
+  defer_inline(a = 1; printf("A is %d\n", a););
+  defer([=] { printf("Should be called first\n"); });
+  auto defer_test = deferred(&nothing);
+}
